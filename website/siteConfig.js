192d59757c8bcde1c74aa996d6f5fd32b0e98655
SporkDaoDev <bob@sporkdao.com>
2021-02-27 14:16:54 -0800

    baseUrl

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,7 +12,7 @@
 const siteConfig = {
   title: 'SporkDao', // Title for your website.
   tagline: 'Docs for SporkDao',
-  url: 'https://sporkdao.github.com', // Your website URL
+  url: 'https://sporkdao.github.com/', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
